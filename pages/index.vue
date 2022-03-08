c82209e64e9156854f13f5c0ef027b4e8b078cff
DTreshy <szczukowski.damian@gmail.com>
2022-03-08 18:08:01 +0100

    initialization

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div class="flex flex-row">
+    <div class="x">
+        <div>
+            sdsdsdsd
+        </div>
+    </div>
+    <div class="basis-3/5"/>
+</div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.x{
+    @apply text-fajnyred-50 basis-2/5 flex justify-center;
+}
+
+</style>
