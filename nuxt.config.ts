baf0090b9328ed664ab498c84384f0424a00e028
DTreshy <szczukowski.damian@gmail.com>
2022-03-19 17:50:04 +0100

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,29 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    build: {
-        postcss: {
-          postcssOptions: {
-            plugins: {
-              tailwindcss: {},
-              autoprefixer: {},
-            },
-          },
+
+  meta: {
+    html: {
+      lang: "pl"
+    },
+  },
+  buildModules: [
+    '@nuxtjs/google-analytics'
+  ],
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID
+  },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
         },
       },
-      css: [
-        "~/assets/css/tailwind.css"
-      ],
+    },
+  },
+  css: [
+    "~/assets/css/tailwind.css"
+  ],
 })
