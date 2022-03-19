3c0b3b849a2fcef98c0877e144f9dec8a73d6c07
Woosaki <kamil.szczukowski@interia.pl>
2022-03-19 21:16:25 +0100

    hover przycisku

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,13 +7,11 @@
         <div to="" class="inline-block lg:p-4 py-2 lg:py-4 lg:text-xl text-sm text-primary-300 font-extrabold float-right">
             Zadzwoń 600 685 804
         </div>
-        <nuxt-link to="kontakt" class=" group hidden float-right mr-20 my-auto">
+        <nuxt-link to="kontakt" class=" group float-right mr-20 my-auto">
           <div class=" p-4 text-xl text-primary-300 font-extrabold">
             Kontakt
           </div>
-          <div class=" ml-3 -mt-4 h-0.5 w-0 group-
-          
-          hover:w-20 transition-all ease-in-out bg-primary-300 rounded-lg" />
+          <div class=" ml-3 -mt-4 h-0.5 w-0 group-hover:w-20 transition-all ease-in-out bg-primary-300 rounded-lg" />
         </nuxt-link>
       </nav>
     <slot />
